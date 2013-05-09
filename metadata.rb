abd82cbbc1662f75293865fa344f897250f924e7
Ryan Richard <ryanrichard07@gmail.com>
2013-05-09 13:23:12 -0500

    first commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'motd'
+maintainer       'Rackspace US, Inc.'
+maintainer_email 'ryan.richard@rackspace.com'
+license          'All rights reserved'
+description      'Installs/Configures motd'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "motd-tail"
