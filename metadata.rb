45d52b5808b00963a5847caa28c259d71e768a83
Ryan Richard <ryanrichard07@gmail.com>
2013-05-09 14:22:02 -0500

    add license, update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,11 @@
 name             'motd'
 maintainer       'Rackspace US, Inc.'
 maintainer_email 'ryan.richard@rackspace.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures motd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
+
+recipe			  "motd", "Updates/adds motd file based on platform family with useful node data"
 
 depends "motd-tail", ">= 1.2.0"
