6726b17190166b682532aa22e970e57c8b06cb0f
Ryan Richard <ryanrichard07@gmail.com>
2014-01-15 13:34:27 -0600

    fix cookbook name, update test-kitchen box location

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
-name             'motd'
+name             'rackspace_motd'
 maintainer       'Rackspace US, Inc.'
 maintainer_email 'rackspace-cookbooks@rackspace.com'
 license          'Apache 2.0'
 description      'Installs/Configures motd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '1.0.0'
 
-recipe		  "motd", "Updates/adds motd file based on platform family with useful node data"
+recipe		  "default.rb", "Updates/adds motd file based on platform family with useful node data"
 
-depends "motd-tail", "~> 2.0"
+depends "rackspace_motd_tail", "~> 2.0"
