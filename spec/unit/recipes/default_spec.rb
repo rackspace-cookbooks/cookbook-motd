7583088b8999327b0683769ec0ef64757e100038
Ryan Richard <ryanrichard07@gmail.com>
2014-01-16 17:24:13 -0600

    debian tests

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,20 +1,42 @@
 require 'spec_helper'
 
 describe 'rackspace_motd::default' do
-  let(:file) {"/etc/motd"}
 
-  let(:chef_run) do
- 	  ChefSpec::Runner.new do |node|
- 		  node.set[:platform_family] = 'rhel'
- 		  node.set[:rackspace_motd][:additional_text] = 'some additional text'
- 	  end.converge(described_recipe)
+  context "platform family - rhel" do
+    let(:file) {"/etc/motd"}
+
+    let(:chef_run) do
+ 	    ChefSpec::Runner.new do |node|
+ 	  	  node.set[:platform_family] = 'rhel'
+ 	  	  node.set[:rackspace_motd][:additional_text] = 'some additional text'
+ 	    end.converge(described_recipe)
+    end
+  
+    it 'writes /etc/motd' do
+ 	    expect(chef_run).to render_file(file).with_content('Chef-Client')
+    end
+  
+    it 'writes /etc/motd with additional text' do
+       expect(chef_run).to render_file(file).with_content('some additional text')
+    end
   end
 
-  it 'writes /etc/motd' do
- 	  expect(chef_run).to render_file(file).with_content('Chef-Client')
+  context "platform family - debian" do
+    let(:file) {"/etc/motd.tail"}
+    let(:chef_run) do
+      ChefSpec::Runner.new do |node|
+        node.set[:platform_family] = 'debian'
+        node.set[:rackspace_motd][:additional_text] = 'some additional text'
+      end.converge(described_recipe)
+    end
+  
+    it 'writes /etc/motd.tail' do
+      expect(chef_run).to render_file(file).with_content('Chef-Client')
+    end
+  
+    it 'writes /etc/motd.tail with additional text' do
+       expect(chef_run).to render_file(file).with_content('some additional text')
+    end
   end
 
-  it 'writes /etc/motd with additional text' do
-     expect(chef_run).to render_file(file).with_content('some additional text')
-  end
 end
