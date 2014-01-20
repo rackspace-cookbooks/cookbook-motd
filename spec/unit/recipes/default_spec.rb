f82c4673e576d2af820d564b1d62f5d3bf0bea75
Ryan Richard <ryanrichard07@gmail.com>
2014-01-20 13:37:23 -0600

    add additional template tests

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'rackspace_motd::default' do
   let(:file) { '/etc/motd' }
+  let(:time) { Time.new }
 
   let(:chef_run) do
     ChefSpec::Runner.new do |node|
@@ -9,7 +10,7 @@ describe 'rackspace_motd::default' do
     end.converge(described_recipe)
   end
 
-  #context 'platform family - rhel' do
+  context 'platform family - rhel' do
 
     it 'creates /etc/motd' do
       expect(chef_run).to create_template(file)
@@ -23,17 +24,39 @@ describe 'rackspace_motd::default' do
     )
     end
 
-  #end
+  end
 
-  #context 'template expectations' do    
+  context 'template expectations' do
 
     it 'should contain node name' do
-      expect(chef_run).to render_file(file).with_content('foo')
+      expect(chef_run).to render_file(file).with_content('Chef-Client: chefspec.local')
     end
 
     it 'writes /etc/motd with additional text' do
-      expect(chef_run).to render_file(file).with_content('some additiona text')
+      expect(chef_run).to render_file(file).with_content('some additional text')
     end
 
-  #end
+    it 'should contain the the chef environment name' do
+      expect(chef_run).to render_file(file).with_content('Environment: _default')
+    end
+
+    it 'should contain the time since most recent chef run' do
+      expect(chef_run).to render_file(file).with_content('Last Run: ' + time.to_s)
+    end
+
+    # ChefSpec::Server isn't working currently
+    # it 'should contain a list of roles' do
+    #  ChefSpec::Server.create_role('fake_role', { default_attributes: {} })
+    #  expect(chef_run).to render_file(file).with_content('Role: fake_role::default')
+    # end
+
+    it 'should contain a list of recipes' do
+      expect(chef_run).to render_file(file).with_content('Recipe: rackspace_motd::default')
+    end
+
+    it 'should contain the chef server url - solo' do
+      expect(chef_run).to render_file(file).with_content('Chef Server: Solo')
+    end
+
+  end
 end
